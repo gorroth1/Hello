ac06076198422048d3d909c60fb0da5ae7286e64
Gabe Orroth <gorroth1@gmail.com>
2015-11-06 19:00:37 -0500

    save

---
+++ java/src/main/java/hello.java
@@ -0,0 +1,19 @@
+/**
+ * This is less then simple then Hello World
+ */
+
+import skilstak.c;
+
+public class hello {
+    
+    public static void plain(String message) {
+        System.out.println(c.clear);
+        while (true) {
+            System.out.print(c.rc() + message + " ");
+        }
+    }
+        
+    public static void main(String[] args) {
+        plain("Hello World!");
+    }
+}
